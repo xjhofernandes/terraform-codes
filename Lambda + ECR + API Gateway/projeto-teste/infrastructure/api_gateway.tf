94d615db742b65b2ff5a62880ef732c7e4ff4d3c
Jonathan Fernandes <xjhofernandes@gmail.com>
2022-02-15 15:17:57 -0300

    :pencil: Configurações do API Gateway

---
+++ Lambda + ECR + API Gateway/projeto-teste/infrastructure/api_gateway.tf
@@ -0,0 +1,63 @@
+resource "aws_api_gateway_rest_api" "example" {
+  name = local.lambda.function_name
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+# Method ANY to root resource
+
+resource "aws_api_gateway_method" "example" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_rest_api.example.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_rest_api.example.root_resource_id
+  http_method = aws_api_gateway_method.example.http_method
+  integration_http_method = "ANY"
+  type = "AWS_PROXY"
+  uri  = aws_lambda_function.container_lambda.invoke_arn
+}
+
+# resource "aws_api_gateway_method_response" "example" {
+#   rest_api_id = aws_api_gateway_rest_api.example.id
+#   resource_id = aws_api_gateway_rest_api.example.root_resource_id
+#   http_method = aws_api_gateway_method.example.http_method
+#   status_code = "200"
+# }
+
+# Proxy Resource
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "ANY"
+  type = "AWS_PROXY"
+  uri  = aws_lambda_function.container_lambda.invoke_arn
+}
+
+# resource "aws_api_gateway_method_response" "proxy" {
+#   rest_api_id = aws_api_gateway_rest_api.example.id
+#   resource_id = aws_api_gateway_resource.proxy.id
+#   http_method = aws_api_gateway_method.example.http_method
+#   status_code = "200"
+# }
