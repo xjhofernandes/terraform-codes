440cd92a7b94447d0cb1a4e2f1e94f29666718f4
Jonathan Fernandes <xjhofernandes@gmail.com>
2022-02-17 15:08:03 -0300

    :pencil: Adicionando endpoint POST teste

--- Lambda + ECR + API Gateway/projeto-teste/main.py
+++ Lambda + ECR + API Gateway/projeto-teste/main.py
@@ -5,8 +5,13 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> str:
     return {"message": "Hello World, from Terraform! :)"}
+
+
+@app.post("/post")
+def teste_post() -> str:
+    return {"message": "Hello World, from POST Terraform! :)"}
 
 
 handler = Mangum(app=app)
